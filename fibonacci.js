82e5c64ac27634b993a2031d10f410c47741a405
tjtrabue <tom.trabue@gmail.com>
2015-02-20 13:26:38 -0600

    Added function signatures for prima and fibonacci

--- fibonacci.js
+++ fibonacci.js
@@ -1,2 +1,8 @@
 /* Please put your code here for how to generate a Fibonacci number.
 */
+
+//This function returns the ith Fibonacci number in the Fiboncacci Sequence.
+//@param index the index of the desired Fibonacci number
+function fibonacci(index) {
+	
+}
--- prime.js
+++ prime.js
@@ -1,2 +1,8 @@
 /* Please put your code here for how to generate a prime number.
 */
+
+//This function returns the ith prime number (determined by the index parameter).
+//@param index the index of the prime number to be generated.
+function prime(index) {
+
+}
