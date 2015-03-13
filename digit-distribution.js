8ea4e6bf1b38b82758db5df70b348e9f776e6f00
tjtrabue <tom.trabue@gmail.com>
2015-03-12 23:42:17 -0500

    Added new file for part 3 of project

---
+++ digit-distribution.js
@@ -0,0 +1,6 @@
+/* 	This file holds code for determining the distribution of all 10 digits 0-9 
+	that comprise an integer in base 10. */ 
+
+//Create a big-integer class
+var bigInt = require("big-integer");
+
--- factorial.js
+++ factorial.js
@@ -30,4 +30,4 @@ function factorial(index) {
 	}
 }
 
-console.log(factorialBigInt(45));
+console.log(factorialBigInt(40));
