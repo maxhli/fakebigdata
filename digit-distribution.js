77d4cb83196fc9f0ce7cb0768a4d07de4de7c3e0
tjtrabue <tom.trabue@gmail.com>
2015-03-19 13:30:15 -0500

    Added functionality for integers prefixed with a "+" or "-"

--- digit-distribution.js
+++ digit-distribution.js
@@ -12,17 +12,32 @@ var bigInt = require("big-integer");
 function digitDistribution(num) {
 	num = num.toString();
 
+	//Check to make sure that the input is an integer:
+	if(isNaN(num))
+		return "Improper input to function digitDistribution. Please input an integer";
+
+	//Check to see if the input contains a "-" or a "+" prefix; if so, remove it before continuing:
+	if(num.indexOf('-') >= 0 || num.indexOf('+') >= 0)
+		num = num.substring(1);
+
+	//Initialize a count array for frequencies of digits 0-9:
 	var countArray = [0,0,0,0,0,0,0,0,0,0];
+	//Initialize a percentage array to be determined by the count array:
 	var percentageArray = [0,0,0,0,0,0,0,0,0,0];
+
+	//Count the frequency of each digit 0-9 in the input number:
 	for(var i = 0; i < num.length; i++) {
 		countArray[num.charAt(i)]++;
 	}
+
+	//Turn counts into percentages to be returned as an array:
 	for(var i = 0; i < 10; i++) {
 		percentageArray[i] = countArray[i] / num.length * 100;
 	}
 
+	//Return the frequency percentages:
 	return percentageArray;
 }
 
 
-console.log(digitDistribution(1234521));
+console.log(digitDistribution("-1020"));
